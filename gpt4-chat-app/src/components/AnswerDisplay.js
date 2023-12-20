e442afd0f006d155d1da6e965ed489dd615ba5a3
Anirudha <anirudhatalmale4@gmail.com>
2023-12-20 18:20:58 +0530

    added icon

--- gpt4-chat-app/src/components/AnswerDisplay.js
+++ gpt4-chat-app/src/components/AnswerDisplay.js
@@ -2,27 +2,32 @@ import React from 'react';
 import '../App.css';
 
 function AnswerDisplay({ role, content, contentType }) {
-
   const createMarkup = (htmlString) => {
     return { __html: htmlString };
   };
 
   return (
     <div className={`message ${role}`}>
-      {role === 'user' && (
-        <>
-          <div className="message-icon">You</div>
-          <br></br>
-          <div className="message-question">{content}</div>
-        </>
-      )}
-      {role === 'assistant' && (
-        <>
-          <div className="message-icon">ChatGPT</div>
-          <br></br>
-          <div className="message-answer" dangerouslySetInnerHTML={createMarkup(content)} />
-        </>
-      )}
+      <div className="message-icon">
+        {/* Here, you can put the icon for the message sender */}
+        {role === 'user' ? <span>U</span> : <span>A</span>}
+      </div>
+      <div className="message-content">
+        {role === 'user' && (
+          <>
+            <div className="message-sender">You</div>
+            <br></br>
+            <div className="message-question">{content}</div>
+          </>
+        )}
+        {role === 'assistant' && (
+          <>
+            <div className="message-sender">ChatGPT</div>
+            <br></br>
+            <div className="message-answer" dangerouslySetInnerHTML={createMarkup(content)} />
+          </>
+        )}
+      </div>
     </div>
   );
 }
