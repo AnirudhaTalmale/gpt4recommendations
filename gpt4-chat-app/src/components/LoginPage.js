34c0d03a6e1d89005ee93bd17ffb69919a7b2140
Anirudha <anirudhatalmale4@gmail.com>
2023-12-24 20:41:26 +0530

    Modifies CSS for google sign in button

--- gpt4-chat-app/src/components/LoginPage.js
+++ gpt4-chat-app/src/components/LoginPage.js
@@ -3,18 +3,18 @@ import '../App.css'; // Assuming you want to use styles from App.css
 
 function LoginPage() {
   const handleLogin = () => {
-    // Redirect to Google OAuth or your authentication page
-    // console.log("Redirect to Google OAuth or your authentication page");
     window.location.href = 'http://localhost:3000/auth/google?prompt=select_account';
   };
 
   return (
     <div className="login-container">
       <button onClick={handleLogin} className="login-button">
-        Login with Google
+        <img src="/icons8-google-logo.svg" alt="Google logo" className="google-logo" />
+        Continue with Google
       </button>
     </div>
   );
 }
 
 export default LoginPage;
+
