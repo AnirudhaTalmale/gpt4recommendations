4ecf2386fd4ea0596a080eaacfe4e0a2ec8172d4
Anirudha <anirudhatalmale4@gmail.com>
2024-01-02 08:27:28 +0530

    saving correct promptTemplate

--- promptTemplate.js
+++ promptTemplate.js
@@ -1,6 +1,14 @@
 // promptTemplate.js
 const bookRecommendationPrompt = (userQuery) => {
-  return `say hi`;
+  return `User query is "${userQuery}". If query not about book recommendations then reply - Please ask question about books. Else give 1 book as per user query exactly in following format based on training data. If user query contains reference to previous chats, then refer them.
+<h3>"Book title by author" enclosed within asterisk</h3>
+<b>Why this book?</b><p>Book relation with user query</p>
+<b>Book summary</b><p>Detailed</p>
+<b>Author's Credibility</b><p>Detailed</p>
+<b>Key Insights</b><p>Detailed</p>
+<b>Case studies and Anecdotes</b><p>Detailed</p>
+<b>Endorsements and Praise</b><p>/p>
+<b>Similar Books</b><ol><li><b>Two "Book title by author" enclosed within asterisk</b><p>Detailed reason for each</p></li></ol>`;
 };
 
 module.exports = bookRecommendationPrompt;
