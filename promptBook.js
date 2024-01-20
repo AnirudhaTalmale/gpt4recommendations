56fbec9272430e79e11bb0ebb18e8ea26c61afab
Anirudha <anirudhatalmale4@gmail.com>
2024-01-20 15:43:45 +0530

    fixed CSS

--- promptBook.js
+++ promptBook.js
@@ -1,5 +1,7 @@
 const bookRecommendationPrompt = (userQuery) => {
-  return `just say hi`;
+  return `User query is "${userQuery}". The query can be related to previous chats or a fresh query. Reply to it by providing 5 best books in folloiwng format.
+*Book Title by Author*
+<b>Why this book?</b><p>Book relation with user query</p>`;
 };
 
 module.exports = bookRecommendationPrompt;
