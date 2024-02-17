ede54ca8643e396f645e877f41f5f703fb29e74d
Anirudha <anirudhatalmale4@gmail.com>
2024-02-17 18:32:01 +0530

    Added the function getAmazonLink

--- promptBook.js
+++
@@ -1,7 +0,0 @@
-const bookRecommendationPrompt = (userQuery) => {
-  return `${userQuery}. Provide 5 books without numbering and strictly in following format:
- Book Title by Author enclosed within the symbol '#'
- <p>Book relation with user query</p>`;
-};
-
-module.exports = bookRecommendationPrompt;
--- promptMoreBooks.js
+++
@@ -1,7 +0,0 @@
-const bookRecommendationPrompt = (userQuery) => {
-    return `${userQuery}. Provide 5 more books, without repeating the previous ones, in given format:
-#Book Title by Author#
-<p>Book relation with user query</p>`;
-};
-  
-module.exports = bookRecommendationPrompt;
