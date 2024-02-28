cd913e8e5b55cc8b0edba1480ed203edb9b15c91
Anirudha <anirudhatalmale4@gmail.com>
2024-02-29 02:12:17 +0530

    Added prompts back

---
+++ promptBook.js
@@ -0,0 +1,7 @@
+const bookRecommendationPrompt = (userQuery) => {
+  return `${userQuery}. Provide 5 books without numbering and in following format:
+  Book Title by Author - should be enclosed within the symbol '#' 
+  <p>Briefly tell why the book is best for given user query</p>`;
+};
+
+module.exports = bookRecommendationPrompt;
