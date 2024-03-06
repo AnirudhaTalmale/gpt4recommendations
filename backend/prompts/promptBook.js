98321a7621cdaaf482c850e0e63750f577adf28b
Anirudha <anirudhatalmale4@gmail.com>
2024-03-07 03:12:57 +0530

    added env variable called BOOK_QUANTITY

--- backend/prompts/promptBook.js
+++ backend/prompts/promptBook.js
@@ -1,5 +1,8 @@
+require('dotenv').config(); // Make sure to require dotenv at the start of your file
+
 const bookRecommendationPrompt = (userQuery) => {
-  return `${userQuery}. Provide 1 book without numbering and in following format:
+  const bookQuantity = process.env.BOOK_QUANTITY;
+  return `${userQuery}. Provide ${bookQuantity} without numbering and in following format:
   Book Title by Author - should be enclosed within the symbol '#' 
   <p>Briefly tell why the book is best for given user query</p>`;
 };
