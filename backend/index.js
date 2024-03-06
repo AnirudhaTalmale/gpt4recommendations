3b41c5851b0159371046e85e0f7a179ebe28d448
Anirudha <anirudhatalmale4@gmail.com>
2024-03-06 10:22:12 +0530

    using app google app password for nodemailer

--- backend/index.js
+++ backend/index.js
@@ -88,8 +88,8 @@ let transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS
-  }
-});
+  } 
+}); 
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
