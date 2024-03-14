97dfe6e978ba0cdb53a393e984a502975b5839bf
Anirudha <anirudhatalmale4@gmail.com>
2024-03-15 04:09:00 +0530

    I have added the book/gallery/book_id endpoint

--- backend/prompts/promptBook.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config(); // Make sure to require dotenv at the start of your file
-
-const bookRecommendationPrompt = (userQuery) => {
-  const bookQuantity = process.env.BOOK_QUANTITY;
-  return `${userQuery}. Provide ${bookQuantity} without numbering and strictly in following format:
-  Book Title by Author - should be enclosed within the symbol '#'`;
-};
-
-module.exports = bookRecommendationPrompt;
