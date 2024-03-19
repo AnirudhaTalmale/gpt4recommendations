1dccc1cb53ff706ff4ca33abb17823493ca45fe7
Anirudha <anirudhatalmale4@gmail.com>
2024-03-19 07:21:11 +0530

    improved video details page

--- backend/prompts/promptAnecdotes.js
+++
@@ -1,7 +0,0 @@
-const anecdotesPrompt = (userQuery) => {
-    return `Book name is "${userQuery}"
- Provide Key Anecdotes strictly in following format:
- <h3>Key Anecdotes</h3><ol><li><strong>Anecdote Name: </strong>Anecdote Story</li></ol>`
-};
-
-module.exports = anecdotesPrompt;
--- backend/prompts/promptKeyInsights.js
+++
@@ -1,7 +0,0 @@
-const keyInsightsPrompt = (userQuery) => {
-    return `Book name is "${userQuery}"
- Provide Key Insights strictly in following format. 
- <h3>Key Insights</h3><ol><li><strong>Key point:</strong>Details</li></ol>`
-};
-
-module.exports = keyInsightsPrompt;
