8d859d57db57e886b989fe322816422b445bb1c8
Anirudha <anirudhatalmale4@gmail.com>
2024-03-27 12:17:46 +0530

    lightbox content css updates

--- backend/prompts/promptKeyInsights.js
+++
@@ -1,7 +0,0 @@
-const keyInsightsPrompt = (userQuery) => {
-    return `Book name is "${userQuery}"
- Provide Key Insights strictly with following html tags:
- <ol><li><b>Key point</b>: Details</li></ol>`
-};
-
-module.exports = keyInsightsPrompt;
