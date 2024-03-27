7fe191a83b0613f154f97dee9f70c69ea083d240
Anirudha <anirudhatalmale4@gmail.com>
2024-03-27 12:44:57 +0530

    test

--- backend/prompts/promptBook.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config(); // Make sure to require dotenv at the start of your file
-
-const bookRecommendationPrompt = (userQuery) => {
-  const bookQuantity = process.env.BOOK_QUANTITY;
-  return `User query is "${userQuery}". For the given user query, provide ${bookQuantity} without numbering and strictly in following format:
-  Book Title by Author - should be enclosed within the symbol '#' `;
-};
-
-module.exports = bookRecommendationPrompt;
