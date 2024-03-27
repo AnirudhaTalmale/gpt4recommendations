987245dfb7e00ad6ac6c9e2d8c3a9c8549215232
Anirudha <anirudhatalmale4@gmail.com>
2024-03-27 20:02:20 +0530

    touch session selection added

--- backend/prompts/promptQuotes.js
+++
@@ -1,7 +0,0 @@
-const quotesPrompt = (userQuery) => {
-    return `Book name is "${userQuery}"
- Provide Key Quotes strictly with following html tags:
- <b>Quotes</b><ol><li>"Quote"</li></ol>`
-};
-
-module.exports = quotesPrompt;
