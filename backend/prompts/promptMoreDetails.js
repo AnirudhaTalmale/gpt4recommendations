19a9ed9256d827d3e440ca0ac78ad24e759a8556
Anirudha <anirudhatalmale4@gmail.com>
2024-03-27 12:28:17 +0530

    lightbox content for book preview max-width reduction for smaller screens

--- backend/prompts/promptMoreDetails.js
+++
@@ -1,8 +0,0 @@
-const moreDetailsPrompt = (userQuery) => {
-    return `${userQuery} strictly with following html tags:
- <b>Book Summary</b><p>Brief</p>
- <b>Author's Credibility</b><p>Brief</p>
- <b>Endorsements and Praise</b><p>Brief</p>`
-};
-
-module.exports = moreDetailsPrompt;
