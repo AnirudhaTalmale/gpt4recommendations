c0007d44c0c0e95c2d925c2abe79a448b69b2ac4
Anirudha <anirudhatalmale4@gmail.com>
2024-03-31 00:28:45 +0530

    updated check format functions

--- backend/prompts/promptAnecdotes.js
+++
@@ -1,7 +0,0 @@
-const anecdotesPrompt = (userQuery) => {
-    return `Book name is "${userQuery}"
- Provide Key Anecdotes strictly with following html tags:
- <b>Anecdotes</b><ol><li><b>Anecdote Name</b>: Anecdote Story</li></ol>`
-};
-
-module.exports = anecdotesPrompt;
--- backend/prompts/promptKeyInsights.js
+++
@@ -1,7 +0,0 @@
-const keyInsightsPrompt = (userQuery) => {
-    return `Book name is "${userQuery}"
- Provide Key Insights strictly with following html tags:
- <b>Insights</b><ol><li><b>Insight name</b>: Insight Description</li></ol>`
-};
-
-module.exports = keyInsightsPrompt;
