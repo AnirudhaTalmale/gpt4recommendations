6e6d77f2d8bcfe3b74294b0cb789241b459fa1be
Anirudha <anirudhatalmale4@gmail.com>
2024-04-06 04:58:34 +0530

    replaced longest common substring logic with a more robust logic using normalisation and delimeter in openaiApi.js

---
+++ frontend/src/components/CommonFunctions.js
@@ -0,0 +1,150 @@
+import axios from 'axios';
+
+export const fetchAnecdotes = async (bookDataObjectId, bookTitle) => {
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/anecdotes`, {
+      params: { bookDataObjectId, bookTitle }
+    });
+    return response.data.anecdotes; 
+  } catch (error) {
+    throw error; 
+  }
+};  
+
+export const handleAnecdotesRequest = async (bookDataObjectId, bookTitle, author, handleQuerySubmit, setIsLightboxOpen, setLightboxContent) => {
+  try {
+    const anecdotes = await fetchAnecdotes(bookDataObjectId, bookTitle);
+    if (!anecdotes) {
+      const userQuery = `${bookTitle} by ${author}`;
+      handleQuerySubmit(userQuery, false, bookDataObjectId, bookTitle, author, false, false, true);
+    } else {
+      setLightboxContent(''); // Reset the content
+      setLightboxContent(anecdotes);
+      setIsLightboxOpen(true);
+    }
+  } catch (error) {
+    const userQuery = `${bookTitle} by ${author}`;
+    handleQuerySubmit(userQuery, false, bookDataObjectId, bookTitle, author, false, false, true);
+  }
+};
+
+export const fetchQuotes = async (bookDataObjectId, bookTitle) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/quotes`, {
+        params: { bookDataObjectId, bookTitle }
+      });
+      return response.data.quotes; 
+    } catch (error) {
+      throw error; 
+    }
+  };  
+  
+  export const handleQuotesRequest = async (bookDataObjectId, bookTitle, author, handleQuerySubmit, setIsLightboxOpen, setLightboxContent) => {
+    try {
+      const quotes = await fetchQuotes(bookDataObjectId, bookTitle);
+      if (!quotes) {
+        const userQuery = `${bookTitle} by ${author}`;
+        handleQuerySubmit(userQuery, false, bookDataObjectId, bookTitle, author, false, false, false, true);
+      } else {
+        setLightboxContent(''); // Reset the content
+        setLightboxContent(quotes);
+        setIsLightboxOpen(true);
+      }
+    } catch (error) {
+      const userQuery = `${bookTitle} by ${author}`;
+      handleQuerySubmit(userQuery, false, bookDataObjectId, bookTitle, author, false, false, false, true);
+    }
+  };
+
+export const fetchKeyInsights = async (bookDataObjectId, bookTitle) => {
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/key-insights`, {
+      params: { bookDataObjectId, bookTitle }
+    });
+    return response.data.keyInsights; 
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const handleKeyInsightsRequest = async (bookDataObjectId, bookTitle, author, handleQuerySubmit, setIsLightboxOpen, setLightboxContent) => {
+  try {
+    const keyInsights = await fetchKeyInsights(bookDataObjectId, bookTitle);
+    if (!keyInsights) {
+      const userQuery = `${bookTitle} by ${author}`;
+      handleQuerySubmit(userQuery, false, bookDataObjectId, bookTitle, author, false, true);
+    } else {
+      setLightboxContent(''); // Reset the content
+      setLightboxContent(keyInsights);
+      setIsLightboxOpen(true);
+    }
+  } catch (error) {
+    const userQuery = `${bookTitle} by ${author}`;
+    handleQuerySubmit(userQuery, false, bookDataObjectId, bookTitle, author, false, true);
+  }
+};
+
+export const fetchMoreDetails = async (bookDataObjectId, bookTitle) => {
+  try {
+    
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/more-details`, {
+      params: { bookDataObjectId, bookTitle }
+    });
+    return response.data.detailedDescription; 
+  } catch (error) {
+    console.log("unable to fetchMoreDetails ")
+  }
+};
+
+export const handleMoreDetailsRequest = async (bookDataObjectId, bookTitle, author, handleQuerySubmit, setIsLightboxOpen, setLightboxContent) => {
+  try {
+    const detailedDescription = await fetchMoreDetails(bookDataObjectId, bookTitle);
+    if (!detailedDescription) {
+      const userQuery = `Explain the book - ${bookTitle} by ${author}`;
+      handleQuerySubmit(userQuery, true, bookDataObjectId, bookTitle, author);
+    } else {
+      setLightboxContent(''); // Reset the content
+      setLightboxContent(detailedDescription);
+      setIsLightboxOpen(true);
+    }
+  } catch (error) {
+    const userQuery = `Explain the book - ${bookTitle} by ${author}`;
+    handleQuerySubmit(userQuery, true, bookDataObjectId, bookTitle, author);
+  }
+};
+
+  export const checkAuthStatus = async () => {
+    try {
+      const authResponse = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/check-auth`, { withCredentials: true });
+  
+      if (authResponse.status === 200 && authResponse.data.isAuthenticated) {
+        if (!authResponse.data.onboardingComplete) {
+          window.location.href = `${process.env.REACT_APP_FRONTEND_URL}/onboarding`;
+          return;
+        }
+  
+        const userInfoResponse = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/user-info`, { withCredentials: true });
+  
+        if (userInfoResponse.status === 200) {
+          return userInfoResponse.data.user; // You might want to return the user data for further use
+        }
+      } else {
+        console.log("Authentication failed, redirecting to login page");
+        window.location.href = `${process.env.REACT_APP_FRONTEND_URL}/auth/login`;
+      }
+    } catch (error) {
+      console.error('Error checking authentication status:', error);
+      window.location.href = `${process.env.REACT_APP_FRONTEND_URL}/auth/login`;
+    }
+  };
+
+  export const renderStarRating = (rating) => {
+    let stars = [];
+    for (let i = 0; i < Math.floor(rating); i++) {
+      stars.push(<i key={`star-${i}`} className="fa fa-star full-star-book-gallery" style={{color: 'orange', fontSize: '0.9rem'}}></i>);
+    }
+    if (rating % 1 !== 0) {
+      stars.push(<i key="half-star" className="fa fa-star-half-stroke half-star-book-gallery" style={{color: 'orange', fontSize: '0.9rem'}}></i>);
+    }
+    return <div style={{display: 'flex', alignItems: 'center', marginRight: '10px'}}>{stars}</div>;
+  };
