471fdda77ab718df1949d763233e9e7ab86019b3
Anirudha <anirudhatalmale4@gmail.com>
2024-06-06 07:17:05 +0530

    blog image size reduced

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,51 @@
+import React, {useEffect} from 'react';
+import HeaderWithHomeButton from './HeaderWithHomeButton'; 
+import '../App.css';
+
+const Contact = () => {
+
+  useEffect(() => {
+    // Create the script element
+    const tawkScript = document.createElement('script');
+    tawkScript.async = true;
+    tawkScript.src = 'https://embed.tawk.to/65fed84aa0c6737bd123ec54/1hplr9hi9';
+    tawkScript.setAttribute('crossorigin', '*');
+
+    // Insert script in the document
+    document.body.appendChild(tawkScript);
+
+    // Remove the script when component unmounts
+    return () => {
+      document.body.removeChild(tawkScript);
+    };
+  }, []);
+
+  return (
+    <>
+     <HeaderWithHomeButton />
+    
+    <div className="contact-container">
+      
+      <h1>Contact us</h1>
+      <div className="contact-buttons-container">
+        <p>
+          <a href="mailto:getbooksai@gmail.com">
+            <i className="fa-solid fa-envelope"></i> Mail
+          </a>
+        </p>
+        <p>
+        <button onClick={() => {
+          if (window.Tawk_API) {
+            window.Tawk_API.maximize();
+          }
+        }}>
+          <i className="fa-solid fa-comments"></i> Chat
+        </button>
+        </p>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Contact;
