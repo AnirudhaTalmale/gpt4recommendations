15e248bdb35d7ec9c49aa9e80a361f84f3919df2
Anirudha <anirudhatalmale4@gmail.com>
2024-06-10 13:24:56 +0530

    updated prompt

--- backend/prompts/promptBook.js
+++ backend/prompts/promptBook.js
@@ -4,11 +4,11 @@ const bookRecommendationPrompt = (userQuery) => {
   let prompt;
 
   if (process.env.NODE_ENV === 'local') {
-    prompt = `User query is "${userQuery}". For the given user query, provide 5 books without numbering and strictly in following format:
+    prompt = `User query is "${userQuery}". For the given user query, provide 5 highly popular books without numbering and strictly in following format:
     Book Title by Author - should be enclosed within the symbol '#' 
     <p>Briefly tell, why the book is best for given user query</p> `;
   } else {
-    prompt = `User query is "${userQuery}". For the given user query, provide 5 books without numbering and strictly in following format:
+    prompt = `User query is "${userQuery}". For the given user query, provide 5 highly popular books without numbering and strictly in following format:
     Book Title by Author - should be enclosed within the symbol '#' 
     <p>Briefly tell, why the book is best for given user query</p> `;
   }
