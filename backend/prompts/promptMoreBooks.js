9993ac76562eca337c531227a5ce460b23fe3df2
Anirudha <anirudhatalmale4@gmail.com>
2024-06-12 06:07:21 +0530

    added blog explaining GetBooks.ai

---
+++ backend/prompts/promptMoreBooks.js
@@ -0,0 +1,19 @@
+require('dotenv').config(); // Make sure to require dotenv at the start of your file
+
+const bookRecommendationPrompt = (userQuery) => {
+  let prompt;
+
+  if (process.env.NODE_ENV === 'local') {
+    prompt = `${userQuery} Provide 5 highly popular books without numbering and other than the previously recommended books, strictly in following format:
+    Book Title by Author - should be enclosed within the symbol '#' 
+    <p>Briefly tell, why the book is best for given user query</p> `;
+  } else {
+    prompt = `${userQuery} Provide 5 highly popular books without numbering and other than the previously recommended books, strictly in following format:
+    Book Title by Author - should be enclosed within the symbol '#' 
+    <p>Briefly tell, why the book is best for given user query</p> `;
+  }
+
+  return prompt;
+};
+
+module.exports = bookRecommendationPrompt;
