ce9651177a1524f6498693e95136670524c28ebc
Anirudha <anirudhatalmale4@gmail.com>
2024-06-13 17:35:23 +0530

    introduced manualStop variable to make sure user don't have to click on the stop streaming button twice

---
+++ backend/prompts/promptMoreBooks.js
@@ -0,0 +1,19 @@
+require('dotenv').config(); // Make sure to require dotenv at the start of your file
+
+const bookRecommendationPrompt = (userQuery) => {
+  let prompt;
+
+  if (process.env.NODE_ENV === 'local') {
+    prompt = `${userQuery} Provide top 5 books without numbering and other than the previously recommended books, strictly in following format:
+    Book Title by Author - should be enclosed within the symbol '#' 
+    <p>Briefly tell, why the book is best for my query</p> `;
+  } else {
+    prompt = `${userQuery} Provide top 5 books without numbering and other than the previously recommended books, strictly in following format:
+    Book Title by Author - should be enclosed within the symbol '#' 
+    <p>Briefly tell, why the book is best for my query</p> `;
+  }
+
+  return prompt;
+};
+
+module.exports = bookRecommendationPrompt;
