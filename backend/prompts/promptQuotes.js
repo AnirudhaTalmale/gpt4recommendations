a775972bfc43ab0b8507217063571dc1ca9f1e2d
Anirudha <anirudhatalmale4@gmail.com>
2024-06-17 22:49:17 +0530

    upgraded the bookPrompt and moreBooksPrompt

---
+++ backend/prompts/promptQuotes.js
@@ -0,0 +1,19 @@
+require('dotenv').config(); // Make sure to require dotenv at the start of your file
+
+const quotesPrompt = (userQuery) => {
+  let prompt;
+
+  if (process.env.NODE_ENV === 'local') {
+    prompt = `Book name is "${userQuery}"
+ Provide large number of important Quotes present in the above book. The final answer should be strictly strictly with following html tags:
+ <b>Quotes</b><ol><li>"Quote"</li></ol>`;
+  } else {
+    prompt = `Book name is "${userQuery}"
+  Provide large number of important Quotes present in the above book. The final answer should be strictly strictly with following html tags:
+ <b>Quotes</b><ol><li>"Quote"</li></ol>`;
+  }
+
+  return prompt;
+};
+
+module.exports = quotesPrompt;
