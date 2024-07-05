ce28fd5bd26251530ce47159e6057d62dc1383c8
Anirudha <anirudhatalmale4@gmail.com>
2024-07-05 11:06:29 +0530

    minor correction in redisClient

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,7 +1,8 @@
 const redis = require('redis');
+const redisURL = process.env.NODE_ENV === 'local' ? process.env.REDIS_URL_LOCAL : process.env.REDIS_URL;
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL
+  url: redisURL
 });
 
 redisClient.connect();
