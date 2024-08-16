caeb1186b1c30c118a6e7bae11caacd57215bcdb
Anirudha <anirudhatalmale4@gmail.com>
2024-08-16 22:21:59 +0530

    added ScrollToTopOnFooterRoutes.js

---
+++ frontend/src/components/ScrollToTopOnFooterRoutes.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+// Define footerRoutes outside the component
+const footerRoutes = ['/about-us', '/blog', '/privacy-policy'];
+
+const ScrollToTopOnFooterRoutes = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (footerRoutes.includes(pathname)) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]); // Only pathname needs to be in the dependency array
+
+  return null;
+};
+
+export default ScrollToTopOnFooterRoutes;
